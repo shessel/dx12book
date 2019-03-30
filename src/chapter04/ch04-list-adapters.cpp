0fec2ec044f4e136db6f1be66b4f11c18943cf01
shessel <shessel@users.noreply.github.com>
2019-03-30 17:49:16 +0100

    add output and display mode info

--- src/chapter04/ch04-list-adapters.cpp
+++ src/chapter04/ch04-list-adapters.cpp
@@ -19,13 +19,51 @@ std::vector<IDXGIAdapter1*> getAdapters()
     return adapters;
 }
 
+std::vector<IDXGIOutput*> getOutputsForAdapter(IDXGIAdapter1* const pAdapter)
+{
+    UINT i = 0;
+    IDXGIOutput* pOutput = nullptr;
+    std::vector<IDXGIOutput*> outputs;
+    while (SUCCEEDED(pAdapter->EnumOutputs(i++, &pOutput)))
+    {
+        outputs.push_back(pOutput);
+    }
+    return outputs;
+}
+
+void printOutputInfo(const std::vector<IDXGIOutput*>& outputs)
+{
+    for (IDXGIOutput* pOutput : outputs)
+    {
+        DXGI_OUTPUT_DESC desc;
+        pOutput->GetDesc(&desc);
+        std::wcout << L"\t" << desc.DeviceName << L"\n";
+
+        UINT displayModeCount = 0;
+        pOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, 0, &displayModeCount, nullptr);
+        std::vector<DXGI_MODE_DESC> displayModes(displayModeCount);
+        pOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, 0, &displayModeCount, displayModes.data());
+
+        for (const DXGI_MODE_DESC& modeDesc : displayModes)
+        {
+            std::wcout << L"\t\t" << modeDesc.Width << L"x" << modeDesc.Height << L"@";
+            std::wcout << static_cast<float>(modeDesc.RefreshRate.Numerator) / modeDesc.RefreshRate.Denominator << L"Hz" << L"\n";
+        }
+    }
+}
+
 void printAdapterInfo(const std::vector<IDXGIAdapter1*>& adapters)
 {
+    bool first = true;
     for (IDXGIAdapter1* pAdapter : adapters)
     {
         DXGI_ADAPTER_DESC1 desc;
         pAdapter->GetDesc1(&desc);
-        std::wcout << desc.Description << L"\n";
+        std::wcout << (first ? L"" : L"\n") << desc.Description << L"\n";
+
+        auto outputs = getOutputsForAdapter(pAdapter);
+        printOutputInfo(outputs);
+        first = false;
     }
 }
 
