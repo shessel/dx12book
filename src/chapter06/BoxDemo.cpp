86113c96a9faf52a19b4d41317e2fd6817b43f8f
shessel <shessel@users.noreply.github.com>
2019-04-07 18:24:49 +0200

    add basic clear/present loop to box demo

--- src/chapter06/BoxDemo.cpp
+++ src/chapter06/BoxDemo.cpp
@@ -153,7 +153,20 @@ void BoxDemo::initialize()
 
 void BoxDemo::render()
 {
-    m_pCommandList->Reset(m_pCommandAllocator.Get(), nullptr);
+    ThrowIfFailed(m_pCommandList->Reset(m_pCommandAllocator.Get(), nullptr));
+
+    {
+        D3D12_RESOURCE_BARRIER presentToRenderTargetTransition = D3D12Util::TransitionBarrier(getCurrentBackBuffer(),
+            D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+        m_pCommandList->ResourceBarrier(1, &presentToRenderTargetTransition);
+    }
+
+    {
+        const float clearColorRgba[] = { 0.0f, 0.0f, 0.5f, 1.0f };
+        m_pCommandList->ClearRenderTargetView(getCurrentBackBufferView(), clearColorRgba, 0, nullptr);
+
+        m_pCommandList->ClearDepthStencilView(getCurrentDepthStencilView(), D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+    }
 
     D3D12_INDEX_BUFFER_VIEW indexBufferView = {};
     indexBufferView.BufferLocation = m_pIndexBuffer->GetGPUVirtualAddress();
@@ -169,6 +182,35 @@ void BoxDemo::render()
 
     m_pCommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
+    {
+        D3D12_VIEWPORT viewport = {};
+        viewport.Width = static_cast<float>(m_windowWidth);
+        viewport.Height = static_cast<float>(m_windowHeight);
+        viewport.MinDepth = 0.0f;
+        viewport.MaxDepth = 1.0f;
+
+        m_pCommandList->RSSetViewports(1, &viewport);
+    }
+
+    {
+        D3D12_RECT scissorRect = {};
+        scissorRect.bottom = m_windowHeight;
+        scissorRect.right = m_windowWidth;
+        m_pCommandList->RSSetScissorRects(1, &scissorRect);
+    }
+
+    {
+        D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = getCurrentBackBufferView();
+        D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle = getCurrentDepthStencilView();
+        m_pCommandList->OMSetRenderTargets(1, &rtvHandle, true, &dsvHandle);
+    }
+
+    {
+        D3D12_RESOURCE_BARRIER renderTargetToPresentTransition = D3D12Util::TransitionBarrier(getCurrentBackBuffer(),
+            D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+        m_pCommandList->ResourceBarrier(1, &renderTargetToPresentTransition);
+    }
+
     m_pCommandList->DrawIndexedInstanced(static_cast<UINT>(m_indices.size()), 1, 0, 0, 0);
     
     ThrowIfFailed(m_pCommandList->Close());
@@ -178,5 +220,9 @@ void BoxDemo::render()
         m_pCommandQueue->ExecuteCommandLists(1, commandLists);
     }
 
+    ThrowIfFailed(m_pSwapChain->Present(0, 0));
+
     flushCommandQueue();
+
+    m_currenBackBufferId = (m_currenBackBufferId + 1) % m_swapChainBufferCount;
 }
