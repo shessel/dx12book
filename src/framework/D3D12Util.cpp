cc6b755c8f2441e45999f06e204d10f10143a384
shessel <shessel@users.noreply.github.com>
2019-04-07 19:07:30 +0200

    add helper for constant buffer

--- src/framework/D3D12Util.cpp
+++ src/framework/D3D12Util.cpp
@@ -1,5 +1,7 @@
 #include "D3D12Util.h"
 
+#include "DebugUtil.h"
+
 namespace D3D12Util
 {
     const D3D12_RESOURCE_BARRIER TransitionBarrier(ID3D12Resource* resource, UINT subresource,
@@ -19,5 +21,42 @@ namespace D3D12Util
         D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter)
     {
         return TransitionBarrier(resource, 0, stateBefore, stateAfter);
+    }
+
+    ConstantBuffer::ConstantBuffer(ID3D12Device* const device, const size_t elementCount, const size_t elementSize)
+    {
+        const size_t elementSizeMultipleOf256 = (elementSize + 0xffull) & (~0xffull);
+        D3D12_RESOURCE_DESC desc = {};
+        desc.Alignment = 0;
+        desc.DepthOrArraySize = 1;
+        desc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+        desc.Flags = D3D12_RESOURCE_FLAG_NONE;
+        desc.Format = DXGI_FORMAT_UNKNOWN;
+        desc.Height = 1;
+        desc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+        desc.MipLevels = 1;
+        desc.SampleDesc.Count = 1;
+        desc.SampleDesc.Quality = 0;
+        desc.Width = static_cast<UINT>(elementCount * elementSize);
+
+        D3D12_HEAP_PROPERTIES heapProperties = {};
+        heapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+        ThrowIfFailed(device->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &desc,
+            D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(m_pUploadBuffer.GetAddressOf())));
+
+        ThrowIfFailed(m_pUploadBuffer->Map(0, nullptr, &m_pMappedBuffer));
+    }
+
+    ConstantBuffer::~ConstantBuffer()
+    {
+        if (m_pUploadBuffer)
+        {
+            m_pUploadBuffer->Unmap(0, nullptr);
+        }
+    }
+
+    void ConstantBuffer::copyData(const void* const data, const size_t dataSize)
+    {
+        memcpy(m_pMappedBuffer, data, dataSize);
     }
 }
--- src/framework/D3D12Util.h
+++ src/framework/D3D12Util.h
@@ -1,4 +1,5 @@
 #include "d3d12.h"
+#include "wrl.h"
 
 namespace D3D12Util
 {
@@ -7,4 +8,19 @@ namespace D3D12Util
 
     const D3D12_RESOURCE_BARRIER TransitionBarrier(ID3D12Resource* resource,
         D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter);
+
+    class ConstantBuffer
+    {
+        ConstantBuffer(ID3D12Device* const device, const size_t elementCount, const size_t elementSize);
+        ~ConstantBuffer();
+
+        ConstantBuffer(const ConstantBuffer& other) = delete;
+        ConstantBuffer& operator=(const ConstantBuffer& other) = delete;
+
+        ID3D12Resource1* getResource() const { return m_pUploadBuffer.Get(); }
+        void copyData(const void* const data, const size_t dataSize);
+    private:
+        Microsoft::WRL::ComPtr<ID3D12Resource1> m_pUploadBuffer;
+        void *m_pMappedBuffer = nullptr;
+    };
 }
