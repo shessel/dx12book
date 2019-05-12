e1e90ff9ae38ad91808da63e1dc6265b6fb828d7
shessel <shessel@users.noreply.github.com>
2019-05-12 18:09:32 +0200

    duplicate land and waves demo as starting point for light demo

---
+++ src/chapter08/ch08-land-and-waves-demo-lit.cpp
@@ -0,0 +1,20 @@
+#include "LandAndWavesDemoLit.h"
+
+#include "DebugUtil.h"
+
+int WinMain(HINSTANCE hInst, HINSTANCE /*prev*/, LPSTR /*argv*/, int /*show*/)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+    try
+    {
+        LandAndWavesDemoLit demo(hInst);
+        return demo.run();
+    }
+    catch (DxDebugException e)
+    {
+        OutputDebugStringDxDebugException(e);
+    }
+    return 0;
+}
